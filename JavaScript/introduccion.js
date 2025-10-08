6475015829962d529b94a3612affef2e2979ec58
SebastianCHB <sebastianchb16@gmail.com>
2025-10-08 14:41:04 -0600

    Diseño

--- JavaScript/introduccion.js
+++ JavaScript/introduccion.js
@@ -1,57 +1,22 @@
-let x = [1,2,3,4]
-const y = [30,50,...x]
-
-console.log(y)
-
-const edaLegalEEUU = 21
-const checkEstado = (edad, edadLegal)=>{
-    return edad >= edadLegal ? "Eres mayor" : "Eres menor"
-}
-
-const persona = checkEstado(18, edaLegalEEUU)
-console.log(persona)
-
-let arreglo = [
-
-    {id:1, name:"Luis", age:20},
-    {id:2, name:"Sebastiano", age:21},
-    {id:3, name:"Anoon", age:20},
-    {id:4, name:"Andres", age:21},
-]
-let ul = document.getElementById("lista") 
-
-var todo = ""
-
-arreglo.forEach((item)=>{
-    item.age = item.age*10
-    todo += `
-         <li>${item.name} - ${item.age}</li>
-    `
-})
-
-ul.innerHTML = todo
-
-
-
-console.log("arreglo original", arreglo);
-var copia = arreglo.map((item, index)=>{
-     item.age *= 10
-        return item
-})
-
-console.log("arreglo modificado", arreglo);
-console.log("arreglo modificado", copia);
-
+var ul = document.getElementById("lista")
 let url = "https://thesimpsonsapi.com/api/"
-fetch(url+"characters").then(response=>{
+var list=""
+let arr_original = []
+
+var getLista = (page)=>{
+fetch(url+"characters?page="+page).then(response=>{
     if(!response.ok){
         console.log("Error en la red")
     }
     return response.json()
 }).then(data=>{
     console.log("Datos del personaje: ",data)
-    var list=""
+   
     data.results.forEach(personaje=>{
+        arr_original.push({
+            personaje:personaje,
+            datos:null
+        })
         list+=`
         <li>
             <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" width="200px">
@@ -59,4 +24,9 @@ fetch(url+"characters").then(response=>{
         </li>   `
     })
     ul.innerHTML = list
+    console.log("Arreglo final",arr_original)
 })
+}
+for(let i=1;i<=5;i++){
+    getLista(i)
+}
