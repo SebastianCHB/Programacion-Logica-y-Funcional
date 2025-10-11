da8a74ae99382c6c85f3388d0c6a5dd2b239ffeb
Sebastian Chacon B <87223451+SebastianCHB@users.noreply.github.com>
2025-10-11 16:33:19 -0600

    Funciones

--- JavaScript/introduccion.js
+++ JavaScript/introduccion.js
@@ -1,62 +1,112 @@
-let x = [1,2,3,4]
-const y = [30,50,...x]
+var ul = document.getElementById("lista")
+let url = "https://thesimpsonsapi.com/api/"
 
-console.log(y)
+let arr_original = []
+var list=""
+var getLista = (page)=>{ 
 
-const edaLegalEEUU = 21
-const checkEstado = (edad, edadLegal)=>{
-    return edad >= edadLegal ? "Eres mayor" : "Eres menor"
+    fetch(url+"characters?page="+page).then(response=>{
+        if(!response.ok){
+            console.log("Error en la red")
+        }
+        return response.json()
+    }).then(data=>{
+        console.log("Datos del personaje: ",data)
+        data.results.forEach(personaje=>{
+            arr_original.push({
+                personaje:personaje,
+                datos:null,
+    
+            })
+            list+=`
+            <li>
+                <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" width="200px">
+                <h2>${personaje.name}</h2>
+            </li>   `
+        })
+        ul.innerHTML = list
+        console.log("ARREGLO: ", arr_original)
+    })
 }
 
-const persona = checkEstado(18, edaLegalEEUU)
-console.log(persona)
+for (let i = 1; i <= 5; i++) {
+    getLista(i)
+}
 
-let arreglo = [
+setTimeout(()=>{
+    document.getElementById("preloader").style.display = "none"
+},1000)
 
-    {id:1, name:"Luis", age:20},
-    {id:2, name:"Sebastiano", age:21},
-    {id:3, name:"Anoon", age:20},
-    {id:4, name:"Andres", age:21},
-]
-let ul = document.getElementById("lista") 
-
-var todo = ""
-
-arreglo.forEach((item)=>{
-    item.age = item.age*10
-    todo += `
-         <li>${item.name} - ${item.age}</li>
-    `
-})
-
-ul.innerHTML = todo
-
-
-
-console.log("arreglo original", arreglo);
-var copia = arreglo.map((item, index)=>{
-     item.age *= 10
-        return item
-})
-
-console.log("arreglo modificado", arreglo);
-console.log("arreglo modificado", copia);
-
-let url = "https://thesimpsonsapi.com/api/"
-fetch(url+"characters").then(response=>{
-    if(!response.ok){
-        console.log("Error en la red")
-    }
-    return response.json()
-}).then(data=>{
-    console.log("Datos del personaje: ",data)
-    var list=""
-    data.results.forEach(personaje=>{
-        list+=`
-        <li>
-            <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" width="200px">
-            <h2>${personaje.name}</h2>
-        </li>   `
+const imprimir =(arr)=>{
+    var todo = "";
+    arr.forEach(item=>{
+        todo+=`
+            <li>
+                <img src="https://cdn.thesimpsonsapi.com/500${item.personaje.portrait_path}" width="200px">
+                <h2>${item.personaje.name}</h2>
+            </li>   `
     })
-    ul.innerHTML = list
+    ul.innerHTML=todo
+}
+//Menores de 18 
+document.querySelector("#linkEdad").addEventListener('click', (evt) => {
+    evt.preventDefault();
+    let res = arr_original.filter(item => item.personaje.age !== null && item.personaje.age < 18);
+    console.log("Menores de 18 (sin null):", res);
+    imprimir(res);
+});
+//Promedio
+document.querySelector("#promedio").addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const personajesConEdad = arr_original.filter(item => item.personaje.age !== null);
+    const sumaDeEdades = personajesConEdad.reduce((acumulador, item) => {
+        return acumulador + item.personaje.age;
+    }, 0);
+    const promedio = sumaDeEdades / personajesConEdad.length;
+    alert(`El promedio de edad es: ${promedio.toFixed(2)} años.`);
+});
+//Fallecidos
+document.querySelector("#fallecidos").addEventListener('click', (evt)=>{
+    evt.preventDefault();
+    let dec = arr_original.filter(item => item.personaje.status == "Deceased");
+    console.log("Fallecidos: ",dec);
+    imprimir(dec);
 })
+
+//Ordenar por edad ASC
+document.querySelector("#opea").addEventListener('click', (evt)=>{
+    evt.preventDefault();
+    const nonull = arr_original.filter(item =>{
+        return item.personaje.age !== null;
+    })
+    const orden = nonull.sort((a,b) => {
+        return a.personaje.age - b.personaje.age;
+    });
+    imprimir(orden)
+})
+
+//Viejo y joven
+document.querySelector("#rucos").addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const soloEdades = arr_original
+        .map(item => item.personaje.age) 
+        .filter(age => age !== null);  
+
+    const edadMasJoven = Math.min(...soloEdades);
+    const edadMasViejo = Math.max(...soloEdades);
+
+    const personajesExtremos = arr_original.filter(item => 
+        item.personaje.age === edadMasJoven || item.personaje.age === edadMasViejo
+    );
+
+    imprimir(personajesExtremos);
+});
+//Estudiantes
+document.querySelector("#Estudiantes").addEventListener('click', evt => {
+    evt.preventDefault();
+    const estudiantes = arr_original.filter(item =>
+        item.personaje.occupation?.toLowerCase().includes("student")
+    );
+    console.log("Son estudiantes: ", estudiantes);
+    imprimir(estudiantes);
+});
